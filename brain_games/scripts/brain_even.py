c4d4e9c59b598727e8ec5bd5f55e101ec06d9cf8
kamusia <adamhorolov36027@gmail.com>
2024-05-06 19:38:37 +0300

    brain-even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,29 @@
+import random
+
+
+def game1():
+    print('answer "yes" if the number is even, otherwise answer "no".')
+    corrects = 0
+    while corrects < 3:
+        num = random.randint(1, 25)
+
+        if num % 2 == 1:
+            correct = 'yes'
+        elif num % 2 == 0:
+            correct = 'no'
+
+        print(f'Question: {num}')
+        answer = input('Your answer: ')
+
+        if answer == correct:
+            corrects += 1
+            print('Correct!')
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct}'.\nLet's try again, Bill!")
+            break
+        if corrects == 3:
+            print('Congratulations, Sam!')
+
+
+if __name__ == '__main__':
+    game1()
