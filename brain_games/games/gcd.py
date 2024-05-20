00637c23a1bae2c9aa10dd694802fac534d501f8
kamusia <adamhorolov36027@gmail.com>
2024-05-20 20:38:34 +0300

    fix gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -10,13 +10,14 @@ def run_game():
 
     question = f'{a} {b}'
 
-    if a == b:
-        correct = str(a)
-
     if a < b:
         a, b = b, a
 
     remainder = a % b
+
+    if remainder == 0:
+        correct = str(b)
+
     while remainder != 0:
         remainder = a % b
         a, b = b, remainder
