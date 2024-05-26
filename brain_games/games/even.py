14d9459526c5e46730fd6fb72c0e1934e11a6e3b
kamusia <adamhorolov36027@gmail.com>
2024-05-26 16:07:52 +0300

    fix

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,14 +4,16 @@ import random
 rules = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
+def is_even(num):
+    if num % 2 == 1:
+        return 'no'
+    elif num % 2 == 0:
+        return 'yes'
+
+
 def run_game():
     num = random.randint(1, 25)
-
-    if num % 2 == 1:
-        correct = 'no'
-    elif num % 2 == 0:
-        correct = 'yes'
-
+    correct = is_even(num)
     question = f'{num}'
 
     return question, correct
--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,15 +4,16 @@ import random
 rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def run_game():
-    num = random.randint(1, 100)
-
-    question = num
-
+def is_prime(num):
     stop = int(num**0.5)
-    correct = 'yes'
     for i in range(2, stop + 1):
         if num % i == 0:
-            correct = 'no'
+            return 'no'
+    return 'yes'
 
+
+def run_game():
+    num = random.randint(1, 100)
+    correct = is_prime(num)
+    question = num
     return question, correct
